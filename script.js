a583ecc0a40bb4dd6ffc21ff1e997ce8601b9969
Shrikanttathe-hub <142688445+Shrikanttathe-hub@users.noreply.github.com>
2024-02-17 00:37:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,35 +1,18 @@
-const promises = [
-  new Promise((resolve) => {
-    setTimeout(() => {
-      resolve("Promise 1 resolved");
-    }, Math.random() * 4000 + 1000);
-  }),
-  new Promise((resolve) => {
-    setTimeout(() => {
-      resolve("Promise 2 resolved");
-    }, Math.random() * 4000 + 1000);
-  }),
-  new Promise((resolve) => {
-    setTimeout(() => {
-      resolve("Promise 3 resolved");
-    }, Math.random() * 4000 + 1000);
-  }),
-  new Promise((resolve) => {
-    setTimeout(() => {
-      resolve("Promise 4 resolved");
-    }, Math.random() * 4000 + 1000);
-  }),
-  new Promise((resolve) => {
-    setTimeout(() => {
-      resolve("Promise 5 resolved");
-    }, Math.random() * 4000 + 1000);
-  }),
-];
+const promises = Array.from({ length: 5 }, getRandomPromise);
 
 Promise.any(promises)
   .then((result) => {
-    document.getElementById("output").innerText = result;
+    document.getElementById('output').innerText = `Resolved with result: ${result}`;
   })
   .catch((error) => {
-    console.error(error);
+    console.error('All promises rejected:', error);
   });
+
+function getRandomPromise() {
+  const randomTime = Math.floor(Math.random() * 5) + 1;
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(randomTime);
+    }, randomTime * 1000);
+  });
+}
