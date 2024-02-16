9587abd53e6b493fd10449f65191743925b4c6bc
Shrikanttathe-hub <142688445+Shrikanttathe-hub@users.noreply.github.com>
2024-02-16 22:54:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-const promises = Array.from({ length: 5 }, getRandomPromise);
+// const promises = Array.from({ length: 5 }, getRandomPromise);
 
-function getRandomPromise() {
-  const delay = Math.floor(Math.random() * 5000) + 1000; // Random delay between 1 and 5 seconds
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(`Promise resolved after ${delay} milliseconds`);
-    }, delay * 1000);
-  });
-}
+// function getRandomPromise() {
+//   const delay = Math.floor(Math.random() * 5000) + 1000; // Random delay between 1 and 5 seconds
+//   return new Promise((resolve) => {
+//     setTimeout(() => {
+//       resolve(`Promise resolved after ${delay} milliseconds`);
+//     }, delay );
+//   });
+// }
 
-Promise.any(promises)
-  .then((result) => {
-    const outputDiv = document.getElementById('output');
-    outputDiv.innerText = result;
-  })
-  .catch((error) => {
-    console.error(error);
-  });
+// Promise.any(promises)
+//   .then((result) => {
+//     const outputDiv = document.getElementById('output');
+//     outputDiv.innerText = result;
+//   })
+//   .catch((error) => {
+//     console.error(error);
+//   });
