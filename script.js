f9047c58a75b0d0ee1da6ac60275764f11953603
Shrikanttathe-hub <142688445+Shrikanttathe-hub@users.noreply.github.com>
2024-02-16 21:57:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,19 @@
-// script.js
-const promises = Array.from({ length: 5 },() => getRandomPromise());
-
-function getRandomTime() {
-    return Math.floor(Math.random() * 5000) + 1000; // Random time between 1 and 5 seconds in milliseconds
-}
+const promises = Array.from({ length: 5 }, getRandomPromise);
 
 function getRandomPromise() {
-    const randomTime = getRandomTime();
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            resolve(`Resolved in ${randomTime} milliseconds`);
-        }, randomTime);
-    });
+  const delay = Math.floor(Math.random() * 5000) + 1000; // Random delay between 1 and 5 seconds
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(`Promise resolved after ${delay} milliseconds`);
+    }, delay);
+  });
 }
 
 Promise.any(promises)
-    .then((result) => {
-        const outputDiv = document.getElementById("output");
-        outputDiv.innerText = result;
-    })
-    .catch((error) => {
-        console.error("All promises failed:", error);
-    });
+  .then((result) => {
+    const outputDiv = document.getElementById('output');
+    outputDiv.innerText = result;
+  })
+  .catch((error) => {
+    console.error(error);
+  });
